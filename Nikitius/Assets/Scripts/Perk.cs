fb78409f87f73efad6cf36efdeeb5ca7722a76cd
Mortumar <73446224+Mortumar@users.noreply.github.com>
2021-03-31 23:54:46 +0200

    31.03 Added astronauts. Setted the collisions. Perks script

---
+++ Nikitius/Assets/Scripts/Perk.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Perk : MonoBehaviour
+{
+
+    [SerializeField] float speed = 10f;
+    [SerializeField] float spin = 10f;
+
+    [SerializeField] Transform target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TargetEarth();
+        Spin();
+    }
+    private void TargetEarth()
+    {
+         transform.position += (target.position - transform.position).normalized * speed * Time.deltaTime; 
+    }
+
+    void Spin()
+    {
+        transform.Rotate(Vector3.up, spin * Time.deltaTime, Space.World);
+        transform.Rotate(Vector3.left, spin * Time.deltaTime, Space.World);
+    }
+
+    private void OnTriggerEnter (Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+        else if (other.gameObject.tag == "Finish")
+        {
+            Debug.Log("Hello, Earth!");
+            Destroy(gameObject);
+        }
+        else if (other.gameObject.tag == "Asteroid")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
